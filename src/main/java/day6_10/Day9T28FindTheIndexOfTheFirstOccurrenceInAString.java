ae20b4406fed137dae177a10eeba69c42c32f2e0
WanTing08 <wwtgwu@gmail.com>
2023-04-12 22:23:43 -0400

    update

---
+++ src/main/java/day6_10/Day9T28FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,17 @@
+package day6_10;
+/*
+    Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
+    or -1 if needle is not part of haystack.
+
+    Example 1:
+    Input: haystack = "sadbutsad", needle = "sad"
+    Output: 0
+    Explanation: "sad" occurs at index 0 and 6.
+    The first occurrence is at index 0, so we return 0.
+ */
+public class Day9T28FindTheIndexOfTheFirstOccurrenceInAString {
+    public int findIndex(String haystack, String needle){
+        if(needle.isEmpty()) return 0;
+
+    }
+}
