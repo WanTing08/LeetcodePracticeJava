7cf16acd2d1436f6357dabf77fc026efbdba2972
WanTing08 <wwtgwu@gmail.com>
2023-04-24 23:35:21 -0400

    update

---
+++ src/main/java/day16_20/Day20T654MaximumBinaryTree.java
@@ -0,0 +1,24 @@
+package day16_20;
+
+import datastructure.TreeNode;
+
+/*
+    You are given an integer array nums with no duplicates.
+    A maximum binary tree can be built recursively from nums using the following algorithm:
+
+    Create a root node whose value is the maximum value in nums.
+    Recursively build the left subtree on the subarray prefix to the left of the maximum value.
+    Recursively build the right subtree on the subarray suffix to the right of the maximum value.
+    Return the maximum binary tree built from nums.
+
+    Example:
+    Input: nums = [3,2,1,6,0,5]
+    Output: [6,3,5,null,2,0,null,null,1]
+ */
+public class Day20T654MaximumBinaryTree {
+    public TreeNode constructTree(int[] nums){
+
+    }
+
+    private TreeNode construct()
+}
